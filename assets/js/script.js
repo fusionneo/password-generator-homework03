40b4f908db8f908aba3f98043a14490d6ef33ea9
fusionneo <fusion.neoseeker@gmail.com>
2022-03-31 23:42:39 -0400

    Added checks for all user criteria and concatenated array based on user password criteria.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", "(", ")", "{", "}", "[", "]", "?", "/", ">", "<", ".", ",", "~", "|"];
+const numberChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,6 +16,13 @@ function writePassword() {
 }
 
 function generatePassword() {
+  var conditionsArray = [];
+  console.log(conditionsArray);
+
+  alert("Let's generate a secure password for you!");
+  alert("You will be prompted for password criteria.\nPlease select at least one character type for your password.")
+
+  //Ask user for password length and validate their input against given criteria.
   var lenPrompt = prompt("How long would you like your password to be? \nPlease enter between 8-128 characters.");
   while (lenPrompt < 8 || lenPrompt > 128 || lenPrompt % 1 !== 0) {
     {
@@ -19,7 +31,9 @@ function generatePassword() {
   }
   console.log(lenPrompt);
 
-  var lowerPrompt = prompt("Would you like lowercase characters? \nPlease enter 'Y' or 'N'.");
+  //Ask user if they want lowercase chars and validate their input against given criteria.
+  //Error checking for lowercase letters.
+  var lowerPrompt = prompt("Would you like LOWERCASE characters? \nPlease enter 'Y' or 'N'.");
   var lowerPromptCheck = lowerPrompt.toUpperCase();
 
   while (lowerPromptCheck != "N" && lowerPromptCheck != "Y") 
@@ -27,6 +41,84 @@ function generatePassword() {
       lowerPrompt = prompt("Please enter 'Y' or 'N'.");
       lowerPromptCheck = lowerPrompt.toUpperCase();
     }
+    console.log("User chose: " + lowerPromptCheck + " for lowercase");
+
+  //Ask user if they want uppercase chars and validate their input against given criteria.
+  //Error checking for lowercase letters.
+  var upperPrompt = prompt("Would you like UPPERCASE characters? \nPlease enter 'Y' or 'N'.");
+  var upperPromptCheck = upperPrompt.toUpperCase();
+
+  while (upperPromptCheck != "N" && upperPromptCheck != "Y") 
+    {
+      upperPrompt = prompt("Please enter 'Y' or 'N'.");
+      upperPromptCheck = upperPrompt.toUpperCase();
+    }
+    console.log("User chose: " + upperPromptCheck + " for uppercase");
+
+  //Ask user if they want numbers and validate their input against given criteria.
+  //Error checking for lowercase letters.
+  var numberPrompt = prompt("Would you like NUMBERS? \nPlease enter 'Y' or 'N'.");
+  var numberPromptCheck = numberPrompt.toUpperCase();
+  
+  while (numberPromptCheck != "N" && numberPromptCheck != "Y") 
+    {
+      var numberPrompt = prompt("Please enter 'Y' or 'N'.");
+      var numberPromptCheck = numberPrompt.toUpperCase();
+    }
+    console.log("User chose: " + numberPromptCheck + " for numbers");
+
+  //Ask user if they want symbols and validate their input against given criteria.
+  //Error checking for lowercase letters.
+  var symbolPrompt = prompt("Would you like SYMBOLS? \nPlease enter 'Y' or 'N'.");
+  var symbolPromptCheck = symbolPrompt.toUpperCase();
+  
+  while (symbolPromptCheck != "N" && symbolPromptCheck != "Y") 
+    {
+      var symbolPrompt = prompt("Please enter 'Y' or 'N'.");
+      var symbolPromptCheck = symbolPrompt.toUpperCase();
+    }
+  console.log("User chose: " + symbolPromptCheck + " for symbols");
+
+
+//If user selected no for all passwords, alert them to try again and return a null value.
+if (lowerPromptCheck == "N" && upperPromptCheck == "N" && numberPromptCheck == "N" && symbolPromptCheck == "N")
+  {
+    alert("You must select at least one character type.\nPlease click Generate Password and try again.");
+    return;
+  }
+
+const userChoice = [];
+
+if (lowerPromptCheck == "Y")
+  {
+    userChoice.push(lowercase)
+  }
+
+if (upperPromptCheck == "Y")
+  {
+    userChoice.push(uppercase)
+  }
+
+if (numberPromptCheck == "Y")
+  {
+    userChoice.push(numberChars)
+  }
+
+if (symbolPromptCheck == "Y")
+  {
+    userChoice.push(specialChars)
+  }
+
+var generatedPassword;
+for (i = 0; i < lenPrompt; i++)
+{
+  x = Math.floor((Math.random() * (userChoice.length)));
+  y = Math.floor((Math.random() * (userChoice[x].length)));
+  var generatedPassword = userChoice[x][y];
+  
+  console.log(generatedPassword);
+}
+
 }
 
 
