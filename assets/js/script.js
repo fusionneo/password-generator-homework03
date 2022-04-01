47407367d7e6694b412fc7a1ea24ed1620399e7c
fusionneo <fusion.neoseeker@gmail.com>
2022-03-31 21:49:38 -0400

    Prompt user for password length and store that value in a variable.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,16 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var lenPrompt = prompt("How long would you like your password to be? \nPlease enter between 8-128 characters.");
+  while (lenPrompt < 8 || lenPrompt > 128 || lenPrompt % 1 !== 0) {
+    {
+      lenPrompt = prompt("Please enter a valid length between 8-128 characters.");
+    }
+  }
+  console.log(lenPrompt);
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
