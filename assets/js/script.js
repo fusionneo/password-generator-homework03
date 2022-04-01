0515855eb8f490a1c87315bee65f72cac8c9ff63
fusionneo <fusion.neoseeker@gmail.com>
2022-03-31 22:15:24 -0400

    Added check for lowercase letters.

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,18 @@ function generatePassword() {
   }
   console.log(lenPrompt);
 
+  var lowerPrompt = prompt("Would you like lowercase characters? \nPlease enter 'Y' or 'N'.");
+  var lowerPromptCheck = lowerPrompt.toUpperCase();
+
+  while (lowerPromptCheck != "N" && lowerPromptCheck != "Y") 
+    {
+      lowerPrompt = prompt("Please enter 'Y' or 'N'.");
+      lowerPromptCheck = lowerPrompt.toUpperCase();
+    }
 }
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
